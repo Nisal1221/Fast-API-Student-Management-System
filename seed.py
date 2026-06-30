06beb06a38cd12c635e1325bd0b163823b9bdcb6
Nisal1221 <nisallakshan123@gmail.com>
2026-06-30 08:04:44 +0530

    edited

--- seed.py
+++ seed.py
@@ -5,3 +5,11 @@ from app.courses.models import Course
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+async def seed_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+        
+        async with AsyncSessionLocal() as session:
+            async with session.begin():
