3b2ea520e6479573892fdbcb0a1c754106a17a87
Nisal1221 <nisallakshan123@gmail.com>
2026-07-03 09:23:31 +0530

    Add authentication router and data validation schemas

---
+++ app/auth/router.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+from fastapi import APIRouter , Depends ,HTTTPexception , status
+from fastaoi.security import OAuth2PasswordRequestForm
+from pydantic importBasemodel , EmailStr
+from aqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from app.database import get_db
+from app.students.models import Student
+
+from app.auth.utils import (
+    hash_password,
+    verify_password,
+    create_access_token,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+)
+
+roter = APIRouter(prefix="/auth" , 
+                  tags=["Authentication"]
+                  )
+
+# 1. TEMPORARY SCHEMAS FOR DATA VALIDATION
+
+class UserRegister(BaseModel):
+    first_name: str
+    last_name:str
+    email:emailStr
+    password:str
+        
+class TokenResponse(BaseModel):
+    access_token:str
+    token_type:str 
